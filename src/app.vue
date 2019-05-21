10920c50d4eef126089034629c67ce1290c10377
Toodyao-XPS-13 <toodyao@gmail.com>
2019-05-21 11:41:23 +0800

    Add framework

---
+++ src/app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div id="cover"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='stylus' scoped>
+#app
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+  #cover
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background-color #999
+    z-index -1
+    opacity 0.9
+</style>
+
---
+++ src/todo/header.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class='main-header'>
+    <h1>Jtodo</h1>
+  </header>
+</template>
+
+
+<style lang="stylus" scoped>
+
+</style>
